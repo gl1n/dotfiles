880a6612554eebfa4cfa4f0257813ac859d4b7a5
lu <nonglu1998@qq.com>
2022-08-24 01:12:01 +0800

    添加自定义snippets

--- nvim/lua/lsp/nvim-cmp.lua
+++ nvim/lua/lsp/nvim-cmp.lua
@@ -1,6 +1,7 @@
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 
 require('luasnip.loaders.from_vscode').lazy_load({"cpp"}) -- 这里需要指明要加载cpp的snippets，否则cpp的不会被加载。其他语言不指明也会被加载
+require('luasnip.loaders.from_vscode').lazy_load({paths = {"~/.config/nvim/my-snippets"}}) -- 指明自定义snippets的路径
 
 local cmp = require('cmp')
 local luasnip = require('luasnip')
