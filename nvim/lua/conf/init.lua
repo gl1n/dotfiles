c8edbabb6d94bf36339f84b212bf3e42cbd51a1c
lu <nonglu1998@qq.com>
2022-08-27 23:27:30 +0800

    删除coc相关

--- nvim/lua/conf/init.lua
+++ nvim/lua/conf/init.lua
@@ -1,7 +1,6 @@
 require('conf.nvim-tree')
 require('conf.nvim-treesitter')
 require('conf.tokyonight')
-require('conf.coc')
 require('conf.lualine')
 require('conf.bufferline')
 require('conf.comment')
