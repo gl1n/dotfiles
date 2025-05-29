9935fe578f62b4ac9a1f0d8b43f1226d941d787a
gl1n <39052367+gl1n@users.noreply.github.com>
2025-05-29 20:40:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,32 +2,25 @@
 
 # 目标目录
 PLUGIN_DIR="$HOME/.zsh/plugins"
-ZSH_AUTOSUGGESTIONS_DIR="$PLUGIN_DIR/zsh-autosuggestions"
-ZSH_SYNTAX_HIGHLIGHTING_DIR="$PLUGIN_DIR/zsh-syntax-highlighting"
-POWERLEVEL10K_DIR="$PLUGIN_DIR/powerlevel10k"
 
-# 创建插件目录
+# 插件列表：格式为 "目录名 git地址"
+PLUGINS="
+zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions
+zsh-syntax-highlighting https://github.com/zsh-users/zsh-syntax-highlighting
+powerlevel10k https://github.com/romkatv/powerlevel10k.git --depth=1
+z https://github.com/rupa/z.git
+"
+
 mkdir -p "$PLUGIN_DIR"
 
-# 下载zsh-autosuggestions
-if [ ! -d "$ZSH_AUTOSUGGESTIONS_DIR" ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_AUTOSUGGESTIONS_DIR"
-else
-  echo "$ZSH_AUTOSUGGESTIONS_DIR 已存在，跳过"
-fi
-
-# 下载zsh-syntax-highlighting
-if [ ! -d "$ZSH_SYNTAX_HIGHLIGHTING_DIR" ]; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_SYNTAX_HIGHLIGHTING_DIR"
-else
-  echo "$ZSH_SYNTAX_HIGHLIGHTING_DIR 已存在，跳过"
-fi
-
-# 下载powerlevel10k
-if [ ! -d "$POWERLEVEL10K_DIR" ]; then
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$POWERLEVEL10K_DIR"
-else
-  echo "$POWERLEVEL10K_DIR 已存在，跳过"
-fi
+echo "$PLUGINS" | while read -r name repo extra; do
+  [ -z "$name" ] && continue
+  dir="$PLUGIN_DIR/$name"
+  if [ ! -d "$dir" ]; then
+    git clone $extra "$repo" "$dir"
+  else
+    echo "$dir 已存在，跳过"
+  fi
+done
 
 echo "全部下载完成！"
